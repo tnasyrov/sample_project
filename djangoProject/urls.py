39ea26e67769b0c33e7c8179a246af744355d0ac
tnasyrov <55081063+tnasyrov@users.noreply.github.com>
2023-06-16 01:28:59 +0300

    django part 2

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -20,5 +20,7 @@ from sample_project import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about', views.about),
+    path('contact', views.contacts)
 ]
