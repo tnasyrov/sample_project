d1e7bcaeda8354c3f4041e46c01381d218cb8418
tnasyrov <55081063+tnasyrov@users.noreply.github.com>
2023-06-16 18:09:18 +0300

    django part 4

--- sample_project/models.py
+++ sample_project/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+
+
+class Order(models.Model):
+    datetime = models.DateTimeField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.PositiveSmallIntegerField()
+    prod_id = models.UUIDField()
+    description = models.TextField()
+
+
+
+products_courses = Product.objects.filter(name__contains='Курс')
+products_lt_2500 = Product.objects.filter(price__lt=2500)
+products_starts_with_course = Product.objects.filter(name__istartswith='курс')
